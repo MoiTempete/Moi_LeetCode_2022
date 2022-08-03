8102f1488865ce8deeda842c9d3633fb6790c448
MoiTempete <moitempete@gmail.com>
2022-08-03 18:54:56 +0800

    problem 14 and problem - find longest common substring

--- src/main/java/moi/leetcode2022/problems/Problem14.java
+++ src/main/java/moi/leetcode2022/problems/Problem14.java
@@ -4,41 +4,8 @@ import moi.leetcode2022.utils.Logger;
 
 import java.util.*;
 
-/*
-14. Longest Common Prefix
-Easy
-
-9387
-
-3237
-
-Add to List
-
-Share
-Write a function to find the longest common prefix string amongst an array of strings.
-
-If there is no common prefix, return an empty string "".
-
-
-
-Example 1:
-
-Input: strs = ["flower","flow","flight"]
-Output: "fl"
-Example 2:
-
-Input: strs = ["dog","racecar","car"]
-Output: ""
-Explanation: There is no common prefix among the input strings.
-
-
-Constraints:
-
-1 <= strs.length <= 200
-0 <= strs[i].length <= 200
-strs[i] consists of only lowercase English letters.
- */
 public class Problem14 {
+
 
     public static String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {
@@ -50,75 +17,38 @@ public class Problem14 {
         if (shortestString.length() == 0) {
             return "";
         }
-        int[] indexes = new int[stringList.size()];
-        Arrays.fill(indexes, -1);
-        int i = shortestString.length() - 1;
-        int offset = 0;
-        int index = -1;
-        StringBuilder result = new StringBuilder();
+        Logger.i("stringList=" + stringList);
+        int i = 0;
         char letter;
-        int findOutCount = 0;
-        String longestString = "";
-
-        while (i - offset  >= 0) {
-            letter = shortestString.charAt(i - offset);
-            indexes[0] = i - offset;
-            Logger.i("\ni=" + i + ", offset=" + offset + ", letter=" + letter + ", current result=" + result);
+        int matchCount = 0;
+        StringBuilder result = new StringBuilder();
+        while (i < shortestString.length()) {
+            letter = shortestString.charAt(i);
             for (int j = 1; j < stringList.size(); j++) {
-                if (indexes[j] < 0) {
-                    index = stringList.get(j).lastIndexOf(String.valueOf(letter));
-                    if (index >= 0) {
-                        indexes[j] = index;
-                        findOutCount++;
-                        Logger.i("find letter " + letter + " in " + stringList.get(j) + " @index=" + index);
-                    } else {
-                        Logger.i("can't find letter" + letter + " in " + stringList.get(j));
-                    }
+                if (stringList.get(j).charAt(i) == letter) {
+                    matchCount ++;
                 } else {
-                    index = indexes[j] - 1;
-                    if (index < stringList.get(j).length() && stringList.get(j).charAt(index) == letter) {
-                        indexes[j] = index;
-                        findOutCount++;
-                        Logger.i("find letter " + letter + " in " + stringList.get(j) + " @index=" + index);
-                    } else {
-                        Logger.i("char at index=" + index + " in " + stringList.get(j) + " is not match with letter=" + letter);
-                    }
+                    break;
                 }
             }
-            if (findOutCount == stringList.size() - 1) {
-                result = new StringBuilder(String.valueOf(letter) + result);
-                Logger.i("find letter " + letter + " in all string, result=" + result);
-                offset++;
+            if (matchCount < stringList.size() - 1) {
+                break;
             } else {
-                i = i - offset - 1;
-                offset = 0;
-                Logger.i("fail somewhere, result=" + result);
-                if (longestString.length() < result.length()) {
-                    longestString = result.toString();
-                }
-                Arrays.fill(indexes, -1);
-                result = new StringBuilder();
+                result.append(letter);
+                i++;
             }
-            findOutCount = 0;
+            matchCount = 0;
         }
-
-        return longestString.length() < result.length() ? String.valueOf(result) : longestString;
+        return result.toString();
     }
 
     public static void test() {
         Map<String[], String> cases = new HashMap<>();
+        cases.put(new String[]{"ab", "a"}, "a");
+        cases.put(new String[]{"cir", "car"}, "c");
+        cases.put(new String[]{"reflower","flow","flight"}, "");
         cases.put(new String[]{"flower","flow","flight"}, "fl");
         cases.put(new String[]{"dog","racecar","car"}, "");
-        cases.put(new String[]{
-                "abcdeffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffgh",
-                "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcdedgh",
-                "ffffffffffffffffffffffffffffffffffffffabcdeffffffffffffffffffffffffffffffffffffff",
-                "abcde"}, "abcde");
-        cases.put(new String[]{
-                "abcdeffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffgh",
-                "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcdedgh",
-                "ffffffffffffffffffffffffffffffffffffffabcdeffffffffffffffffffffffffffffffffffffff",
-                "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcde"}, "ffffffffffffffffffffffffffffffffffffff");
 
         for (Map.Entry<String[], String> entry : cases.entrySet()) {
             String[] input = entry.getKey();
---
+++ src/main/java/moi/leetcode2022/problems/ProblemCommonString.java
@@ -0,0 +1,95 @@
+package moi.leetcode2022.problems;
+
+import moi.leetcode2022.utils.Logger;
+
+import java.util.*;
+
+public class ProblemCommonString {
+
+    public static String longestCommonString(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        List<String> stringList = Arrays.asList(strs);
+        stringList.sort(Comparator.comparingInt(String::length));
+        String shortestString = stringList.get(0);
+        if (shortestString.length() == 0) {
+            return "";
+        }
+        int[] indexes = new int[stringList.size()];
+        Arrays.fill(indexes, -1);
+        int i = shortestString.length() - 1;
+        int offset = 0;
+        int index = -1;
+        StringBuilder result = new StringBuilder();
+        char letter;
+        int findOutCount = 0;
+        String longestString = "";
+
+        while (i - offset  >= 0) {
+            letter = shortestString.charAt(i - offset);
+            indexes[0] = i - offset;
+            for (int j = 1; j < stringList.size(); j++) {
+                if (indexes[j] < 0) {
+                    index = stringList.get(j).lastIndexOf(String.valueOf(letter));
+                    if (index >= 0) {
+                        indexes[j] = index;
+                        findOutCount++;
+                    }
+                } else {
+                    index = indexes[j] - 1;
+                    if (index < stringList.get(j).length() && stringList.get(j).charAt(index) == letter) {
+                        indexes[j] = index;
+                        findOutCount++;
+                    }
+                }
+            }
+            if (findOutCount == stringList.size() - 1) {
+                result = new StringBuilder(String.valueOf(letter) + result);
+                offset++;
+            } else {
+                i = i - offset - 1;
+                offset = 0;
+                if (longestString.length() <= result.length()) {
+                    longestString = result.toString();
+                }
+                Arrays.fill(indexes, -1);
+                result = new StringBuilder();
+            }
+            findOutCount = 0;
+        }
+
+        return longestString.length() <= result.length() ? String.valueOf(result) : longestString;
+    }
+
+    public static void test() {
+        Map<String[], String> cases = new HashMap<>();
+        cases.put(new String[]{"reflower","flow","flight"}, "fl");
+        cases.put(new String[]{"cir", "car"}, "c");
+        cases.put(new String[]{"flower","flow","flight"}, "fl");
+        cases.put(new String[]{"dog","racecar","car"}, "");
+        cases.put(new String[]{
+                "abcdeffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffgh",
+                "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcdedgh",
+                "ffffffffffffffffffffffffffffffffffffffabcdeffffffffffffffffffffffffffffffffffffff",
+                "abcde"}, "abcde");
+        cases.put(new String[]{
+                "abcdeffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffgh",
+                "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcdedgh",
+                "ffffffffffffffffffffffffffffffffffffffabcdeffffffffffffffffffffffffffffffffffffff",
+                "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcde"}, "ffffffffffffffffffffffffffffffffffffff");
+
+        for (Map.Entry<String[], String> entry : cases.entrySet()) {
+            String[] input = entry.getKey();
+            String expect = entry.getValue();
+            Logger.i("input=" + Arrays.toString(input) + ", except=" + expect);
+            String output = longestCommonString(input);
+            if (output.equals(expect)) {
+                Logger.i("case pass by output=" + output);
+            } else {
+                throw new AssertionError("case fail by:" + "\ninput=" + Arrays.toString(input) + ", expect=" + expect + ", but output=" + output);
+            }
+        }
+        Logger.i("All Pass");
+    }
+}
