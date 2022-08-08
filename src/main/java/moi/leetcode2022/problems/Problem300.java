55e1ad927b28c74466aa862d98a1edaf4286b25c
MoiTempete <moitempete@gmail.com>
2022-08-08 11:59:45 +0800

    Problem300

---
+++ src/main/java/moi/leetcode2022/problems/Problem300.java
@@ -0,0 +1,95 @@
+package moi.leetcode2022.problems;
+
+import moi.leetcode2022.utils.Logger;
+
+import java.util.*;
+
+/*
+300. Longest Increasing Subsequence
+Medium
+
+13495
+
+251
+
+Add to List
+
+Share
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+
+Constraints:
+
+1 <= nums.length <= 2500
+-10^4 <= nums[i] <= 10^4
+
+ */
+public class Problem300 {
+
+    public static int lengthOfLIS(int[] nums) {
+        ArrayList<Integer> lis = new ArrayList<>();
+        for (int num : nums) {
+            int size = lis.size();
+            if (size == 0 || num > lis.get(size - 1)) {
+                lis.add(num);
+            } else {
+                lis.set(binarySearch(lis, num), num);
+            }
+        }
+
+        return lis.size();
+    }
+
+    public static int binarySearch(List<Integer> list, int target) {
+        int left = 0;
+        int right = list.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    public static void test() {
+        Map<int[], Integer> cases = new HashMap<>();
+        cases.put(new int[]{10, 9, 2, 5, 3, 7, 101, 18}, 4);
+        cases.put(new int[]{0, 1, 0, 3, 2, 3}, 4);
+        cases.put(new int[]{7, 7, 7, 7, 7, 7, 7}, 1);
+
+        for (Map.Entry<int[], Integer> entry : cases.entrySet()) {
+            int[] input = entry.getKey();
+            int expect = entry.getValue();
+            Logger.i("input=" + Arrays.toString(input));
+            int output = lengthOfLIS(input);
+            if (output == expect) {
+                Logger.i("case pass by output=" + output);
+            } else {
+                throw new AssertionError("case fail by:" + "\ninput=" + Arrays.toString(input) + ", expect=" + expect + ", but output=" + output);
+            }
+        }
+        Logger.i("All Pass");
+    }
+}
