ad5905fa20511997104e9b133a5b721d467fd828
MoiTempete <moitempete@gmail.com>
2022-08-26 11:29:39 +0800

    Complete structure and util

--- src/main/java/moi/leetcode2022/structure/TreeNode.java
+++ src/main/java/moi/leetcode2022/structure/TreeNode.java
@@ -1,6 +1,11 @@
 package moi.leetcode2022.structure;
 
+import moi.leetcode2022.utils.Logger;
 import moi.leetcode2022.utils.TreeNodeUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class TreeNode {
     public int val;
@@ -33,6 +38,42 @@ public class TreeNode {
                     ((this.right == null && ((TreeNode) obj).right == null) || (this.right != null && this.right.equals(((TreeNode) obj).right)));
         } else {
             return false;
+        }
+    }
+
+    Stack<TreeNode> visit = new Stack<>();
+    List<TreeNode> seen = new ArrayList<>();
+
+    public void dfsTraversal() {
+        visit.push(this);
+        seen.add(this);
+        dfs(this);
+        Logger.ic("dfs path ");
+        for (int i = 0; i < seen.size(); i++) {
+            if (i == 0) {
+                Logger.ic("= [" + seen.get(i).val + "]");
+            } else {
+                Logger.ic(", [" + seen.get(i).val + "]");
+            }
+        }
+    }
+
+    private void dfs(TreeNode node) {
+        TreeNode nextNode =
+                node.left != null && !seen.contains(node.left)
+                ? node.left
+                : (node.right != null && !seen.contains(node.right)
+                        ? node.right
+                        : null);
+        if (nextNode != null) {
+            visit.push(nextNode);
+            seen.add(nextNode);
+            dfs(nextNode);
+        } else {
+            visit.pop();
+            if (!visit.empty()) {
+                dfs(visit.peek());
+            }
         }
     }
 }
