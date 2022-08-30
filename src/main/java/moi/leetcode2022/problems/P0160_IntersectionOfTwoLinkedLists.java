a9bcec5f789483ca8c8d383f7996d60852cc0617
MoiTempete <moitempete@gmail.com>
2022-08-30 17:52:48 +0800

    Problem 0160 -  Intersection of Two Linked Lists.

---
+++ src/main/java/moi/leetcode2022/problems/P0160_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,118 @@
+package moi.leetcode2022.problems;
+
+import moi.leetcode2022.structure.ListNode;
+import moi.leetcode2022.utils.ListNodeUtil;
+import moi.leetcode2022.utils.Logger;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+
+
+Example 1:
+
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+- Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+Example 2:
+
+
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+Example 3:
+
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: No intersection
+Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+
+
+Constraints:
+
+The number of nodes of listA is in the m.
+The number of nodes of listB is in the n.
+1 <= m, n <= 3 * 10^4
+1 <= Node.val <= 10^5
+0 <= skipA < m
+0 <= skipB < n
+intersectVal is 0 if listA and listB do not intersect.
+intersectVal == listA[skipA] == listB[skipB] if listA and listB intersect.
+
+
+Follow up: Could you write a solution that runs in O(m + n) time and use only O(1) memory?
+ */
+public class P0160_IntersectionOfTwoLinkedLists {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        /*
+         ListNode A = 1, 2, 3, 4, 5, 6
+         ListNode B = 7, 8, 4, 5, 6
+         If there is an intersection part, it must at tail
+         So let
+         A = A + B = 1, 2, 3, 4, 5, 6, 7, 8, 4, 5, 6
+         B = B + A = 7, 8, 4, 5, 6, 1, 2, 3, 4, 5, 6
+         When A[i] = B[i], the two list start to be intersection
+         */
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+        if (ptrA == null || ptrB == null) {
+            return null;
+        }
+        while (ptrA != ptrB) {
+            ptrA = ptrA == null ? headB : ptrA.next;
+            ptrB = ptrB == null ? headA : ptrB.next;
+        }
+        return ptrA;
+    }
+
+    public static void test() {
+        Map<ListNode[], ListNode> cases = new HashMap<>();
+        cases.put(new ListNode[]{ListNodeUtil.of("[4,2,8,4,5]"), ListNodeUtil.of("[5,6,1,8,4,5]")}, ListNodeUtil.of("8,4,5]"));
+
+        for (Map.Entry<ListNode[], ListNode> entry : cases.entrySet()) {
+            ListNode[] input = entry.getKey();
+            ListNode expect = entry.getValue();
+            Logger.i("input=" + Arrays.toString(input) + ", except=" + expect);
+            ListNode output = getIntersectionNode(input[0], input[1]);
+            if (Objects.equals(output, expect)) {
+                Logger.i("case pass by output=" + output);
+            } else {
+                throw new AssertionError("case fail by:" + "\nexpect=" + expect + ", but output=" + output);
+            }
+        }
+        Logger.i("All Pass");
+    }
+}
